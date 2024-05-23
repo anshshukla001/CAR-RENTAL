da17a35e01e1ff73ae59f043ef6ba22db556f279
Ansh Shukla <anshshukla516@gmail.com>
2024-05-23 22:46:37 +0530

    first commit

---
+++ car-rental-main-master/src/Pages/About.jsx
@@ -0,0 +1,72 @@
+import Footer from "../components/Footer";
+import HeroPages from "../components/HeroPages";
+import PlanTrip from "../components/PlanTrip";
+import AboutMain from "../images/about/about-main.jpg";
+import Box1 from "../images/about/icon1.png";
+import Box2 from "../images/about/icon2.png";
+import Box3 from "../images/about/icon3.png";
+
+function About() {
+  return (
+    <>
+      <section className="about-page">
+        <HeroPages name="About" />
+        <div className="container">
+          <div className="about-main">
+            <img
+              className="about-main__img"
+              src={AboutMain}
+              alt="car-renting"
+            />
+            <div className="about-main__text">
+              <h3>About Company</h3>
+              <h2>You start the engine and your adventure begins</h2>
+              <p>
+              What sets us apart is our commitment to convenience and affordability. With easy online booking and flexible rental options, we strive to make your experience as seamless as possible. Plus, our competitive rates ensure that you get the best value for your money.
+              </p>
+              <div className="about-main__text__icons">
+                <div className="about-main__text__icons__box">
+                  <img src={Box1} alt="car-icon" />
+                  <span>
+                    <h4>20</h4>
+                    <p>Car Types</p>
+                  </span>
+                </div>
+                <div className="about-main__text__icons__box">
+                  <img src={Box2} alt="car-icon" />
+                  <span>
+                    <h4>85</h4>
+                    <p>Rental Outlets</p>
+                  </span>
+                </div>
+                <div className="about-main__text__icons__box">
+                  <img src={Box3} alt="car-icon" className="last-fk" />
+                  <span>
+                    <h4>75</h4>
+                    <p>Repair Shop</p>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <PlanTrip />
+        </div>
+      </section>
+      <div className="book-banner">
+        <div className="book-banner__overlay"></div>
+        <div className="container">
+          <div className="text-content">
+            <h2>Book a car by getting in touch with us</h2>
+            <span>
+              <i className="fa-solid fa-phone"></i>
+              <h3>95462-54263</h3>
+            </span>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default About;
---
+++ car-rental-main-master/src/components/Testimonials.jsx
@@ -0,0 +1,62 @@
+import Img2 from "../images/testimonials/pfp1.jpg";
+import Img3 from "../images/testimonials/pfp2.jpg";
+
+function Testimonials() {
+  return (
+    <>
+      <section className="testimonials-section">
+        <div className="container">
+          <div className="testimonials-content">
+            <div className="testimonials-content__title">
+              <h4>Reviewed by People</h4>
+              <h2>Client's Testimonials</h2>
+            </div>
+
+            <div className="all-testimonials">
+              <div className="all-testimonials__box">
+                <span className="quotes-icon">
+                  <i className="fa-solid fa-quote-right"></i>
+                </span>
+                <p>
+                  "We rented a car from this website and had an amazing
+                  experience! The booking was easy and the rental rates were
+                  very affordable. "
+                </p>
+                <div className="all-testimonials__box__name">
+                  <div className="all-testimonials__box__name__profile">
+                    <img src={Img2} alt="user_img" />
+                    <span>
+                      <h4>Rohit</h4>
+                      <p>Delhi</p>
+                    </span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="all-testimonials__box box-2">
+                <span className="quotes-icon">
+                  <i className="fa-solid fa-quote-right"></i>
+                </span>
+                <p>
+                  "The car was in great condition and made our trip even better.
+                  Highly recommend for this car rental website!"
+                </p>
+                <div className="all-testimonials__box__name">
+                  <div className="all-testimonials__box__name__profile">
+                    <img src={Img3} alt="user_img" />
+                    <span>
+                      <h4>Salman </h4>
+                      <p>Zirakpur</p>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Testimonials;
